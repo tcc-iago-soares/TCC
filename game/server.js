eb8e05dc6e5d060dfa9ae7cbaf47e0115f71e6b7
iagosoares95 <iagosoaressantos@gmail.com>
2019-03-25 13:48:20 -0300

    Sinalização

---
+++ game/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/game.html');
+});
+
+server.listen(process.env.PORT || 8082,function(){
+    console.log('Listening on '+server.address().port);
+});
+
