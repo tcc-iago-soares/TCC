3486c637baeef5a0c9e5bf4c63990eb19d1af3a2
Ederson Torresini <boidacarapreta@gmail.com>
2019-03-25 14:13:15 -0300

    Coisas de vscode

--- sinalization/ua.js
+++ sinalization/ua.js
@@ -3,9 +3,9 @@ window.document.body.appendChild(remoteAudio);
 
 var socket = new JsSIP.WebSocketInterface('wss://sip.myhost.com');
 var configuration = {
-  sockets  : [ socket ],
-  uri      : 'sip:iago@example.com',
-  password : 'impostoeroubo'
+  sockets: [socket],
+  uri: 'sip:iago@example.com',
+  password: 'impostoeroubo'
 };
 
 var ua = new JsSIP.UA(configuration);
@@ -13,23 +13,23 @@ var ua = new JsSIP.UA(configuration);
 ua.start();
 
 var eventHandlers = {
-  'progress': function(e) {
+  'progress': function (e) {
     console.log('call is in progress');
   },
-  'failed': function(e) {
-    console.log('call failed with cause: '+ e.data.cause);
+  'failed': function (e) {
+    console.log('call failed with cause: ' + e.data.cause);
   },
-  'ended': function(e) {
-    console.log('call ended with cause: '+ e.data.cause);
+  'ended': function (e) {
+    console.log('call ended with cause: ' + e.data.cause);
   },
-  'confirmed': function(e) {
+  'confirmed': function (e) {
     console.log('call confirmed');
   }
 };
 
 var options = {
-  'eventHandlers'    : eventHandlers,
-  'mediaConstraints' : { 'audio': true, 'video': true }
+  'eventHandlers': eventHandlers,
+  'mediaConstraints': { 'audio': true, 'video': true }
 };
 
 var session = ua.call('someoone', options);
